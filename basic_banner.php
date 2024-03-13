35777f233f6aef26e471a8698b1acf51f1c36c26
nullstep <me@nullstep.com>
2024-03-13 00:12:42 +0000

    update updater

--- basic_banner.php
+++ basic_banner.php
@@ -3,10 +3,10 @@
 /*
  * Plugin Name: basic_banner
  * Plugin URI: https://nullstep.com/wp-plugins
- * Description: A simple banner/slider
+ * Description: a simple banner/slider
  * Author: nullstep
  * Author URI: https://nullstep.com
- * Version: 1.2.2
+ * Version: 1.2.3
  */
 
 defined('ABSPATH') or die('⎺\_(ツ)_/⎺');
@@ -1436,8 +1436,8 @@ if (!class_exists('WPU')) {
 		private $authorize_token;
 		private $github_response;
 
-		public $requires;
-		public $tested;
+		private $requires;
+		private $tested;
 
 		public function __construct($file) {
 			$this->file = $file;
@@ -1450,6 +1450,11 @@ if (!class_exists('WPU')) {
 			$this->plugin = get_plugin_data($this->file);
 			$this->basename = plugin_basename($this->file);
 			$this->active = is_plugin_active($this->basename);
+		}
+
+		public function set_versions($requires, $tested) {
+			$this->requires = $requires;
+			$this->tested = $tested;
 		}
 
 		public function set_username($username) {
@@ -1597,8 +1602,7 @@ define('_BB', _bbSettings::get_settings());
 
 if (get_option('auth_key') !== '') {
 	$updater = new WPU(__FILE__);
-	$updater->$requires = '6.4';
-	$updater->$tested = '6.4.3';
+	$updater->set_versions('6.4', '6.4.3');
 	$updater->set_username('nullstep');
 	$updater->set_repository('basic_banner');
 	$updater->authorize(get_option('auth_key'));
